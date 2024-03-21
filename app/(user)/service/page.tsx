7b414e72d298a6fa26ea7530f0bbc8527f7e595a
sangsokea <sangsokea109@gmail.com>
2024-03-21 10:06:52 +0700

    fetch data on client

--- app/(user)/service/page.tsx
+++ app/(user)/service/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+"use client";
+import CardProduct from "@/components/card/CardProduct";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const ENDPOINT = "https://fakestoreapi.com/products/";
 
 export default function page() {
-  return (
-    <div className='h-screen grid place-content-center text-6xl'>Service</div>
-  )
+	const [products, setProducts] = useState([]);
+	const router = useRouter();
+
+	useEffect(() => {
+		fetch(ENDPOINT)
+			.then((res) => res.json())
+			.then((data) => setProducts(data));
+	}, []);
+	return (
+		<div className="h-screen flex flex-wrap justify-center gap-3">
+			{products.map((product: any, index) => (
+				<CardProduct
+          onClick={() => router.push(`/service/${product.id}`)}
+					key={index}
+					title={product.title}
+					image={product.image}
+					price={product.price}
+				/>
+			))}
+		</div>
+	);
 }
