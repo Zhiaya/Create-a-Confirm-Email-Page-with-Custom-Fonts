55404dab3b67e3112ed54d91ea3343e1c19d4df1
sangsokea <sangsokea109@gmail.com>
2024-03-21 09:26:23 +0700

    fetch data on server

--- app/(user)/service/[id]/page.tsx
+++ app/(user)/service/[id]/page.tsx
@@ -1,13 +1,34 @@
+import CardComponent from "@/components/card/CardProductDetail";
+
 type PropsParams = {
-    params: {
-        id: string;
-    };
-    searchParams: any
+	params: {
+		id: number;
+	};
+	searchParams: any;
 };
 
+const ENDPOINT = "https://fakestoreapi.com/products/";
 
-export default function Detail(props: PropsParams) {
-    console.log("This is props: ", props)
-    console.log(props.searchParams.productName)
-	return <div className='h-screen grid place-content-center text-6xl'>{props.params.id}</div>;
+export const getData = async (id: number) => {
+	const res = await fetch(`${ENDPOINT}${id}`);
+	const data = await res.json();
+	console.log(data);
+	return data;
+};
+
+export default async function Detail(props: PropsParams) {
+	let data = await getData(props.params.id);
+
+	return (
+		<div className="h-screen grid place-content-center">
+			<CardComponent
+				title={data?.title || "NoTitle"}
+				description={data?.description || "No Description"}
+				image={
+					data?.image ||
+					"https://i0.wp.com/sunrisedaycamp.org/wp-content/uploads/2020/10/placeholder.png?ssl=1"
+				}
+			/>
+		</div>
+	);
 }
